8f9824f30f03efd1a2b30e60d20e3a7c4363b426
Vireon <on96852@gmail.com>
2026-07-08 04:27:40 +0400

    Update social-links.js

--- config/components/social-links.js
+++ config/components/social-links.js
@@ -1,1 +1,82 @@
-.
+import CONFIG from "../config.js";
+
+
+class SocialLinks {
+
+
+    static render() {
+
+
+        const container =
+            document.querySelector(
+                "#social-links"
+            );
+
+
+        if (!container) {
+            return;
+        }
+
+
+
+        container.innerHTML = "";
+
+
+
+        CONFIG.socials.forEach(
+            social => {
+
+
+                const link =
+                    document.createElement(
+                        "a"
+                    );
+
+
+                link.className =
+                    "social-card";
+
+
+                link.href =
+                    social.url;
+
+
+                link.target =
+                    "_blank";
+
+
+
+                link.rel =
+                    "noopener noreferrer";
+
+
+
+                link.innerHTML = `
+
+                    <img
+                        src="assets/icons/${social.icon}.svg"
+                        alt="${social.name}"
+                    >
+
+                    <span>
+                        ${social.name}
+                    </span>
+
+                `;
+
+
+
+                container.appendChild(
+                    link
+                );
+
+            }
+        );
+
+    }
+
+
+}
+
+
+export default SocialLinks;
