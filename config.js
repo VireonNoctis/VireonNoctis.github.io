9bbe641705e59ac58b0713513df3a3098888970c
Vireon <on96852@gmail.com>
2026-07-08 04:15:40 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,75 @@
-.
+const CONFIG = {
+    profile: {
+        name: "Vireon",
+        title: "Imperial Archive",
+        description: "Developer • Reader • Creator",
+
+        avatar: "assets/avatar.png",
+        banner: "assets/banner.png"
+    },
+
+    socials: [
+        {
+            name: "GitHub",
+            url: "https://github.com/VireonNoctis",
+            icon: "github"
+        },
+        {
+            name: "Discord",
+            url: "https://discord.com/users/1419744000977403994",
+            icon: "discord"
+        },
+        {
+            name: "AniList",
+            url: "https://anilist.co/user/Vireon",
+            icon: "anilist"
+        },
+        {
+            name: "Telegram",
+            url: "https://t.me/Theslopking",
+            icon: "telegram"
+        },
+        {
+            name: "Last.fm",
+            url: "https://last.fm/user/reasfeetadmirer",
+            icon: "lastfm"
+        }
+    ],
+
+    github: {
+        username: "VireonNoctis"
+    },
+
+    anilist: {
+        username: "Vireon"
+    },
+
+    lastfm: {
+        username: "reasfeetadmirer",
+        apiKey: "8c06a8eea595aa5e4b748ca235fd9dd8"
+    },
+
+    stats: {
+        showAnime: true,
+        showManga: true,
+        showChapters: true,
+        showGithub: true,
+        showMusic: true
+    },
+
+    theme: {
+        name: "Imperial Gold System",
+
+        colors: {
+            gold: "#D4AF37",
+            crimson: "#8B0000",
+            jade: "#2E8B78",
+            ink: "#090909",
+            paper: "#D8C7A5"
+        }
+    },
+
+    api: {
+        cacheTime: 3600
+    }
+};
