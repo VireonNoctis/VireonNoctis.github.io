6282b3cee80c76bed557c0765fd3aa7c6faba38c
Vireon <on96852@gmail.com>
2026-07-08 04:57:36 +0400

    Update social-links.js

--- config/components/social-links.js
+++ config/components/social-links.js
@@ -1,65 +1,18 @@
+// components/social-links.js
 import CONFIG from "../config.js";
 
-
 class SocialLinks {
+  static render(socials = CONFIG.socials) {
+    const container = document.querySelector("#social-links");
+    if (!container) return;
 
-
-    static render(socials = CONFIG.socials) {
-
-
-        const container =
-            document.querySelector(
-                "#social-links"
-            );
-
-
-
-        if (!container) {
-
-            return;
-
-        }
-
-
-
-
-
-        container.innerHTML = socials
-            .map(
-                social => `
-
-                <a
-                    class="social-card"
-                    href="${social.url}"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-
-
-                    <img
-                        src="${CONFIG.assets.icons}${social.icon}.svg"
-                        alt="${social.name}"
-                    >
-
-
-                    <span>
-                        ${social.name}
-                    </span>
-
-
-                </a>
-
-                `
-            )
-            .join("");
-
-
-
-    }
-
-
+    container.innerHTML = socials.map((social) => `
+      <a class="social-card" href="${social.url}" target="_blank" rel="noopener noreferrer">
+        <img src="${CONFIG.assets.icons}${social.icon}.svg" alt="${social.name}">
+        <span>${social.name}</span>
+      </a>
+    `).join("");
+  }
 }
-
-
 
 export default SocialLinks;
