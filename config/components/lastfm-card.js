618293022f5c9f6331be9a62a624fdf4bb748b0f
Vireon <on96852@gmail.com>
2026-07-08 04:29:55 +0400

    Update lastfm-card.js

--- config/components/lastfm-card.js
+++ config/components/lastfm-card.js
@@ -1,1 +1,63 @@
+class LastFMCard {
 
+
+    static render(data) {
+
+
+        const container =
+            document.querySelector(
+                "#lastfm-card"
+            );
+
+
+        if (!container || !data) {
+            return;
+        }
+
+
+
+        container.innerHTML = `
+
+
+            <img
+                class="album-cover"
+                src="${data.avatar}"
+                alt="LastFM"
+            >
+
+
+
+            <h4>
+                ${data.playing
+                    ? "Now Playing"
+                    : "Last Played"
+                }
+            </h4>
+
+
+
+            <p>
+                ${data.track || ""}
+            </p>
+
+
+            <p>
+                ${data.artist || ""}
+            </p>
+
+
+
+            <p>
+                Scrobbles:
+                ${data.scrobbles || "Unknown"}
+            </p>
+
+
+        `;
+
+    }
+
+}
+
+
+export default LastFMCard;
