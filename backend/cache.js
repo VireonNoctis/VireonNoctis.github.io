1ce91a08d11d7c54e20c56908126b4d08895950c
Vireon <on96852@gmail.com>
2026-07-08 04:17:01 +0400

    Update cache.js

--- backend/cache.js
+++ backend/cache.js
@@ -1,1 +1,40 @@
+class Cache {
+    static storage = new Map();
 
+
+    static set(key, value, duration = 3600000) {
+        this.storage.set(key, {
+            value,
+            expires: Date.now() + duration
+        });
+    }
+
+
+    static get(key) {
+        const item = this.storage.get(key);
+
+        if (!item) {
+            return null;
+        }
+
+        if (Date.now() > item.expires) {
+            this.storage.delete(key);
+            return null;
+        }
+
+        return item.value;
+    }
+
+
+    static has(key) {
+        return this.get(key) !== null;
+    }
+
+
+    static clear() {
+        this.storage.clear();
+    }
+}
+
+
+export default Cache;
